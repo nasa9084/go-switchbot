902ec44080900c56227a71bc1ccb8b8fe083ebef
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2022-11-06 00:37:04 +0900

    support parse webhook event

--- webhook.go
+++ webhook.go
@@ -1,8 +1,13 @@
 package switchbot
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 type WebhookService struct {
@@ -141,4 +146,297 @@ func (svc *WebhookService) Delete(ctx context.Context, url string) error {
 	defer resp.Close()
 
 	return nil
+}
+
+func deviceTypeFromWebhookRequest(r *http.Request) (string, error) {
+	var rawBody bytes.Buffer
+	var deviceTypeBody struct {
+		Context struct {
+			DeviceType string `json:"deviceType"`
+		} `json:"context"`
+	}
+
+	if err := json.NewDecoder(io.TeeReader(r.Body, &rawBody)).Decode(&deviceTypeBody); err != nil {
+		return "", err
+	}
+
+	r.Body = io.NopCloser(&rawBody)
+
+	return deviceTypeBody.Context.DeviceType, nil
+}
+
+type MotionSensorEvent struct {
+	EventType    string                   `json:"eventType"`
+	EventVersion string                   `json:"eventVersion"`
+	Context      MotionSensorEventContext `json:"context"`
+}
+
+type MotionSensorEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the motion state of the device, "DETECTED" stands for motion is detected;
+	// "NOT_DETECTED" stands for motion has not been detected for some time
+	DetectionState string `json:"detectionState"`
+}
+
+type ContactSensorEvent struct {
+	EventType    string                    `json:"eventType"`
+	EventVersion string                    `json:"eventVersion"`
+	Context      ContactSensorEventContext `json:"context"`
+}
+
+type ContactSensorEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the motion state of the device, "DETECTED" stands for motion is detected;
+	// "NOT_DETECTED" stands for motion has not been detected for some time
+	DetectionState string `json:"detectionState"`
+	// when the enter or exit mode gets triggered, "IN_DOOR" or "OUT_DOOR" is returned
+	DoorMode string `json:"doorMode"`
+	// the level of brightness, can be "bright" or "dim"
+	Brightness string `json:"brightness"`
+	// the state of the contact sensor, can be "open" or "close" or "timeOutNotClose"
+	OpenState string `json:"openState"`
+}
+
+type MeterEvent struct {
+	EventType    string            `json:"eventType"`
+	EventVersion string            `json:"eventVersion"`
+	Context      MeterEventContext `json:"context"`
+}
+
+type MeterEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	Temperature float64 `json:"temperature"`
+	Scale       string  `json:"scale"`
+	Humidity    int     `json:"humidity"`
+}
+
+type MeterPlusEvent struct {
+	EventType    string                `json:"eventType"`
+	EventVersion string                `json:"eventVersion"`
+	Context      MeterPlusEventContext `json:"context"`
+}
+
+type MeterPlusEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	Temperature float64 `json:"temperature"`
+	Scale       string  `json:"scale"`
+	Humidity    int     `json:"humidity"`
+}
+
+type LockEvent struct {
+	EventType    string           `json:"eventType"`
+	EventVersion string           `json:"eventVersion"`
+	Context      LockEventContext `json:"context"`
+}
+
+type LockEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the state of the device, "LOCKED" stands for the motor is rotated to locking position;
+	// "UNLOCKED" stands for the motor is rotated to unlocking position; "JAMMED" stands for
+	// the motor is jammed while rotating
+	LockState string `json:"lockState"`
+}
+
+type IndoorCamEvent struct {
+	EventType    string                `json:"eventType"`
+	EventVersion string                `json:"eventVersion"`
+	Context      IndoorCamEventContext `json:"context"`
+}
+
+type IndoorCamEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the detection state of the device, "DETECTED" stands for motion is detected
+	DetectionState string `json:"detectionState"`
+}
+
+type PanTiltCamEvent struct {
+	EventType    string                 `json:"eventType"`
+	EventVersion string                 `json:"eventVersion"`
+	Context      PanTiltCamEventContext `json:"context"`
+}
+
+type PanTiltCamEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the detection state of the device, "DETECTED" stands for motion is detected
+	DetectionState string `json:"detectionState"`
+}
+
+type ColorBulbEvent struct {
+	EventType    string                `json:"eventType"`
+	EventVersion string                `json:"eventVersion"`
+	Context      ColorBulbEventContext `json:"context"`
+}
+
+type ColorBulbEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the current power state of the device, "ON" or "OFF"
+	PowerState string `json:"powerState"`
+	// the brightness value, range from 1 to 100
+	Brightness int `json:"brightness"`
+	// the color value, in the format of RGB value, "255:255:255"
+	Color string `json:"color"`
+	// the color temperature value, range from 2700 to 6500
+	ColorTemperature int `json:"colorTemperature"`
+}
+
+type StripLightEvent struct {
+	EventType    string                 `json:"eventType"`
+	EventVersion string                 `json:"eventVersion"`
+	Context      StripLightEventContext `json:"context"`
+}
+
+type StripLightEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the current power state of the device, "ON" or "OFF"
+	PowerState string `json:"powerState"`
+	// the brightness value, range from 1 to 100
+	Brightness int `json:"brightness"`
+	// the color value, in the format of RGB value, "255:255:255"
+	Color string `json:"color"`
+}
+
+type PlugMiniJPEvent struct {
+	EventType    string                 `json:"eventType"`
+	EventVersion string                 `json:"eventVersion"`
+	Context      PlugMiniJPEventContext `json:"context"`
+}
+
+type PlugMiniJPEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the current power state of the device, "ON" or "OFF"
+	PowerState string `json:"powerState"`
+}
+
+type PlugMiniUSEvent struct {
+	EventType    string                 `json:"eventType"`
+	EventVersion string                 `json:"eventVersion"`
+	Context      PlugMiniUSEventContext `json:"context"`
+}
+
+type PlugMiniUSEventContext struct {
+	DeviceType   string `json:"deviceType"`
+	DeviceMac    string `json:"deviceMac"`
+	TimeOfSample int64  `json:"timeOfSample"`
+
+	// the current power state of the device, "ON" or "OFF"
+	PowerState string `json:"powerState"`
+}
+
+func ParseWebhookRequest(r *http.Request) (interface{}, error) {
+	deviceType, err := deviceTypeFromWebhookRequest(r)
+	if err != nil {
+		return nil, err
+	}
+
+	switch deviceType {
+	case "WoPresence":
+		// Motion Sensor
+		var event MotionSensorEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoContact":
+		// Contact Sensor
+		var event ContactSensorEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoLock":
+		// Lock
+		var event LockEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoCamera":
+		// Indoor Cam
+		var event IndoorCamEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoPanTiltCam":
+		// Pan/Tilt Cam
+		var event PanTiltCamEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoBulb":
+		// Color Bulb
+		var event ColorBulbEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoStrip":
+		// LED Strip Light
+		var event StripLightEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoPlugUS":
+		// Plug Mini (US)
+		var event PlugMiniUSEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoPlugJP":
+		// Plug Mini (JP)
+		var event PlugMiniJPEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoMeter":
+		// Meter
+		var event MeterEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	case "WoMeterPlus":
+		// Meter Plus
+		var event MeterPlusEvent
+		if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+			return nil, err
+		}
+		return &event, nil
+	default:
+		return nil, fmt.Errorf("unknown device type: %s", deviceType)
+	}
 }
--- webhook_test.go
+++ webhook_test.go
@@ -1,6 +1,7 @@
 package switchbot_test
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"net/http"
@@ -179,4 +180,387 @@ func TestWebhookDelete(t *testing.T) {
 	if err := c.Webhook().Delete(context.Background(), "url1"); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestParseWebhook(t *testing.T) {
+	sendWebhook := func(url, req string) {
+		http.DefaultClient.Post(url, "application/json", bytes.NewBufferString(req))
+	}
+
+	t.Run("motion sensor", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.MotionSensorEvent); ok {
+					want := switchbot.MotionSensorEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.MotionSensorEventContext{
+							DeviceType:     "WoPresence",
+							DeviceMac:      "01:00:5e:90:10:00",
+							DetectionState: "NOT_DETECTED",
+							TimeOfSample:   123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a motion sensor event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context": {"deviceType":"WoPresence","deviceMac":"01:00:5e:90:10:00","detectionState":"NOT_DETECTED","timeOfSample":123456789}}`)
+	})
+
+	t.Run("contact sensor", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.ContactSensorEvent); ok {
+					want := switchbot.ContactSensorEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.ContactSensorEventContext{
+							DeviceType:     "WoContact",
+							DeviceMac:      "01:00:5e:90:10:00",
+							DetectionState: "NOT_DETECTED",
+							DoorMode:       "OUT_DOOR",
+							Brightness:     "dim",
+							OpenState:      "open",
+							TimeOfSample:   123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a contact sensor event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoContact","deviceMac":"01:00:5e:90:10:00","detectionState":"NOT_DETECTED","doorMode":"OUT_DOOR","brightness":"dim","openState":"open","timeOfSample":123456789}}`)
+	})
+
+	t.Run("meter", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.MeterEvent); ok {
+					want := switchbot.MeterEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.MeterEventContext{
+							DeviceType:   "WoMeter",
+							DeviceMac:    "01:00:5e:90:10:00",
+							Temperature:  22.5,
+							Scale:        "CELSIUS",
+							Humidity:     31,
+							TimeOfSample: 123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a meter event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoMeter","deviceMac":"01:00:5e:90:10:00","temperature":22.5,"scale":"CELSIUS","humidity":31,"timeOfSample":123456789}}`)
+	})
+
+	t.Run("meter plus", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.MeterPlusEvent); ok {
+					want := switchbot.MeterPlusEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.MeterPlusEventContext{
+							DeviceType:   "WoMeterPlus",
+							DeviceMac:    "01:00:5e:90:10:00",
+							Temperature:  22.5,
+							Scale:        "CELSIUS",
+							Humidity:     31,
+							TimeOfSample: 123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a meter plus event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		// in the request body example the deviceType is Meter but I think it should be WoMeterPlus
+		// https://github.com/OpenWonderLabs/SwitchBotAPI/blob/main/README-v1.0.md#meter-plus
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoMeterPlus","deviceMac":"01:00:5e:90:10:00","temperature":22.5,"scale":"CELSIUS","humidity":31,"timeOfSample":123456789}}`)
+	})
+
+	t.Run("lock", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.LockEvent); ok {
+					want := switchbot.LockEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.LockEventContext{
+							DeviceType:   "WoLock",
+							DeviceMac:    "01:00:5e:90:10:00",
+							LockState:    "LOCKED",
+							TimeOfSample: 123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a lock event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoLock","deviceMac":"01:00:5e:90:10:00","lockState":"LOCKED","timeOfSample":123456789}}`)
+	})
+
+	t.Run("indoor cam", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.IndoorCamEvent); ok {
+					want := switchbot.IndoorCamEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.IndoorCamEventContext{
+							DeviceType:     "WoCamera",
+							DeviceMac:      "01:00:5e:90:10:00",
+							DetectionState: "DETECTED",
+							TimeOfSample:   123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a camera event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoCamera","deviceMac":"01:00:5e:90:10:00","detectionState":"DETECTED","timeOfSample":123456789}}`)
+	})
+
+	t.Run("pan/tilt cam", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.PanTiltCamEvent); ok {
+					want := switchbot.PanTiltCamEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.PanTiltCamEventContext{
+							DeviceType:     "WoPanTiltCam",
+							DeviceMac:      "01:00:5e:90:10:00",
+							DetectionState: "DETECTED",
+							TimeOfSample:   123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a pan/tilt camera event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoPanTiltCam","deviceMac":"01:00:5e:90:10:00","detectionState":"DETECTED","timeOfSample":123456789}}`)
+	})
+
+	t.Run("color bulb", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.ColorBulbEvent); ok {
+					want := switchbot.ColorBulbEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.ColorBulbEventContext{
+							DeviceType:       "WoBulb",
+							DeviceMac:        "01:00:5e:90:10:00",
+							PowerState:       "ON",
+							Brightness:       10,
+							Color:            "255:245:235",
+							ColorTemperature: 3500,
+							TimeOfSample:     123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a color bulb event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoBulb","deviceMac":"01:00:5e:90:10:00","powerState":"ON","brightness":10,"color":"255:245:235","colorTemperature":3500,"timeOfSample":123456789}}`)
+	})
+
+	t.Run("led strip light", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.StripLightEvent); ok {
+					want := switchbot.StripLightEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.StripLightEventContext{
+							DeviceType:   "WoStrip",
+							DeviceMac:    "01:00:5e:90:10:00",
+							PowerState:   "ON",
+							Brightness:   10,
+							Color:        "255:245:235",
+							TimeOfSample: 123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a LED strip light event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoStrip","deviceMac":"01:00:5e:90:10:00","powerState":"ON","brightness":10,"color":"255:245:235","timeOfSample":123456789}}`)
+	})
+
+	t.Run("plug mini (US)", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.PlugMiniUSEvent); ok {
+					want := switchbot.PlugMiniUSEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.PlugMiniUSEventContext{
+							DeviceType:   "WoPlugUS",
+							DeviceMac:    "01:00:5e:90:10:00",
+							PowerState:   "ON",
+							TimeOfSample: 123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a plug mini (US) event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoPlugUS","deviceMac":"01:00:5e:90:10:00","powerState":"ON","timeOfSample":123456789}}`)
+	})
+
+	t.Run("plug mini (JP)", func(t *testing.T) {
+		srv := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				event, err := switchbot.ParseWebhookRequest(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if got, ok := event.(*switchbot.PlugMiniJPEvent); ok {
+					want := switchbot.PlugMiniJPEvent{
+						EventType:    "changeReport",
+						EventVersion: "1",
+						Context: switchbot.PlugMiniJPEventContext{
+							DeviceType:   "WoPlugJP",
+							DeviceMac:    "01:00:5e:90:10:00",
+							PowerState:   "ON",
+							TimeOfSample: 123456789,
+						},
+					}
+
+					if diff := cmp.Diff(want, *got); diff != "" {
+						t.Fatalf("event mismatch (-want +got):\n%s", diff)
+					}
+				} else {
+					t.Fatalf("given webhook event must be a plug mini (JP) event but %T", event)
+				}
+			}),
+		)
+		defer srv.Close()
+
+		sendWebhook(srv.URL, `{"eventType":"changeReport","eventVersion":"1","context":{"deviceType":"WoPlugJP","deviceMac":"01:00:5e:90:10:00","powerState":"ON","timeOfSample":123456789}}`)
+	})
 }
